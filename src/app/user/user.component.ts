caf1aa6ece1ba94e00ef3c59390c1fff29c33982
Shubham Vast <shubhamvast5@gmail.com>
2024-07-13 15:46:49 +0530

    Task component creation and sending data from user component to task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,36 +1,46 @@
-import { Component , computed, EventEmitter, input, Input, output, Output, signal} from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-
 export class UserComponent {
   //#without required (below one are signal)
   // @Input() avatar!:string;
   // @Input() name!:string;
   // avatar = input<string>();
   // name = input<string>();
-
-//#with required  (below on are signal)
-  @Input({required:true}) id!:string;
-  @Input({required:true}) avatar!:string;
-  @Input({required:true}) name!:string;
-//   avatar = input.required<string>();
-//   name = input.required<string>();
-//   imagePath = computed(()=>{
-//    return 'assets/users/'+this.avatar()
-// });
-//  @Output() select = new EventEmitter();
-  select = output<string>();
-  get imagePath(){
-    return 'assets/users/'+this.avatar;
+  @Input({ required: true }) user!: User;
+  //#with required  (below on are signal)
+  //   avatar = input.required<string>();
+  //   name = input.required<string>();
+  //   imagePath = computed(()=>{
+  //    return 'assets/users/'+this.avatar()
+  // });
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
   }
 
   // selectedUser = DUMMY_USERS[randomIndex];
@@ -43,7 +53,7 @@ export class UserComponent {
   // }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
