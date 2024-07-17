49644f3e71334be7f42ab1a09533e0fe82f6a7ec
Shubham Vast <shubhamvast5@gmail.com>
2024-07-17 11:01:06 +0530

    Added NewTask component to add task just a plain component feature will get added in upcoming commit

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { UserComponent } from '../user/user.component';
+import { HeaderComponent } from '../header/header.component';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, UserComponent, HeaderComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) userId!:string;
-  @Input({ required: true })  name!: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask: boolean = false;
+
   tasks = [
     {
       id: 't1',
@@ -37,11 +42,15 @@ export class TasksComponent {
     },
   ];
 
-  get selectedUserTasks(){
-    return this.tasks.filter((task)=> task.userId === this.userId)
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
 
-  onCompleteTask(id: string){
-    this.tasks = this.tasks.filter((task)=> task.id !== id);
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
